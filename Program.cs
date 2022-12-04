fe5ac4502b7df501ce39c89e2a75e12f5402cb1f
Anna <sviridova_anya@mail.ru>
2022-12-04 20:05:31 +0300

    Программа, которая определяет, является ли число палиндромом

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите натуральное число N: ");
+int number = int.Parse (Console.ReadLine());
+string str = number.ToString();
+if (str.Reverse().SequenceEqual(str))
+{
+Console.WriteLine("Введенное число является палиндромом");
+}
+else 
+{
+Console.WriteLine("Введенное число не является палиндромом");
+}
